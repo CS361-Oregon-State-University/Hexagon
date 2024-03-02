6d5061de892512bca04ad37880251b27e4d6003a
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2024-03-01 23:52:13 -0800

    added fastapi and client interactions

---
+++ server/classes.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class workoutApp:
+    def __init__(self):
+        self.username = "admin"
+        self.password = "admin"
+        self.hasworkedout = False
+
+    def whatIsMyName(self):
+        return self.username
+
+    def updateUsername(self, username):
+        self.username = username
+
+
+class workoutPlan:
+    def __init__(self, planName, completion, planGoal):
+        self.planName = planName
+        self.completion = completion
+        self.planGoal = planGoal
+
+    def setCompletion(self, newCompletion):
+        self.completion = newCompletion
+
+    def getCompletion(self):
+        return self.completion
+
+    def setGoal(self, newGoal):
+        self.planGoal = newGoal
+
+    def getGoal(self):
+        return self.planGoal
+
+
+workoutAppInstance = workoutApp()
+
+
+@app.get("/test")
+async def test():
+    return {"message": "test worked"}
+
+
+@app.get("/username")
+async def getUsername():
+    return {"username": {workoutAppInstance.whatIsMyName()}}
+
+
+class Username(BaseModel):
+    username: str
+
+
+@app.post("/updateUsername")
+async def updateUsername(username: Username):
+    workoutAppInstance.updateUsername(username.username)
+
+
+@app.get("/loginInfo")
+async def checkLogin():
+    return {"username": workoutAppInstance.username,
+            "password": workoutAppInstance.password}
