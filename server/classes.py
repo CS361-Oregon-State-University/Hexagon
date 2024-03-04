27f2d531ba04d944dc0352d68befdb64af327573
ali-akturin <ali.akturin2@gmail.com>
2024-03-04 14:24:30 -0800

    Created getters and setters for User class, set member variables

--- server/classes.py
+++ server/classes.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import datetime
 app = FastAPI()
 
 
@@ -35,6 +35,105 @@ class workoutPlan:
     def getGoal(self):
         return self.planGoal
 
+
+class Injury:
+    def __init__(self): #TODO 
+        self.injury = ""
+
+class Device:
+    def __init__(self):  #TODO 
+        self.device = ""
+
+class User:
+    def __init__(self):
+        self.__firstName = ""
+        self.__lastName = ""
+        self.__email = ""
+        self.__password = ""
+        self.__sex = ''
+        self.__gender = ''
+        self.__weight = 0
+        self.__height = 0
+        self.__birthday = datetime.datetime(0, 0, 0) #year, month, day
+        self.__experienceLevels = [] #int[]
+        self.__injuryList = Injury()
+        self.__deviceList = Device() #
+    
+    def getFirstName(self) -> str: 
+        return self.__firstName
+    
+    def setFirstName(self, firstName: str) -> None:
+        self.__firstName = firstName
+
+    def getLastName(self) -> str:
+        return self.__lastName
+
+    def setLastName(self, lastName: str) -> None:
+        self.__lastName = lastName
+    
+    def getEmail(self) -> str:
+        return self.__email
+    
+    def setEmail(self, email: str) -> None:
+        self.__email = email
+    
+    def getPassword(self) -> str:
+        return self.__password
+
+    def setPassword(self, password) -> None:
+        self.__password 
+    
+    def getSex(self) -> str:
+        return self.__sex
+
+    def setSex(self, sex: str) -> None:
+        self.__sex = sex
+    
+    def getGender(self) -> str:
+        return self.__gender
+
+    def setGender(self, gender: str) -> None:
+        self.__gender = gender
+    
+    def getWeight(self) -> int:
+        return self.__weight
+    
+    def setWeight(self, weight: int) -> None:
+        self.__weight = weight
+    
+    def getHeight(self) -> int:
+        return self.__height
+
+    def setHeight(self, height) -> None:
+        self.__height = height
+    
+    def getBirthday(self) -> datetime.datetime:
+        return self.__birthday
+    
+    def setBirthday(self, year: int, month: int, day: int) -> None:
+        self.__birthday = datetime.datetime(year, month, day)
+    
+    def getExperienceLevels(self) -> list[int]:
+        return self.__experienceLevels
+    
+    def setExperienceLevels(self, experienceLevels: list[int]) -> None: #could make a different setter
+        self.__experienceLevels = experienceLevels
+    
+    def getInjuryList(self) -> list[Injury]:
+        return self.__injuryList
+    
+    def setInjuryList(self, injuryList: list[Injury]) -> None:
+        self.__injuryList = injuryList
+    
+    def getDeviceList(self) -> list[Device]:
+        return self.__deviceList
+
+    def setDeviceList(self, deviceList: list[Device]) -> None:
+        self.__deviceList = deviceList
+
+    
+
+        
 
 
 workoutAppInstance = workoutApp()
