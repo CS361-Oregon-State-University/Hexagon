30de4c5331853dc58741c1d0494f1f8dbc69cbb1
ali-akturin <ali.akturin2@gmail.com>
2024-03-18 19:15:12 -0700

    Finished Timer component

--- client/src/routes/startWorkout.tsx
+++ client/src/routes/startWorkout.tsx
@@ -1,24 +1,28 @@
 import Navbar from "../components/navbar";
 import { useUser } from "@clerk/clerk-react";
 import { CSSProperties } from 'react';
+import { MyTimer } from "../components/timer";
 import ExerciseCard from "../components/exerciseCard";
 
 const startWorkout = () => {
     const username = useUser().user?.username;
     const userImg = useUser().user?.imageUrl;
     const userId = useUser().user?.id;
-
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+    
     return (
         <>
             <Navbar />
             <div className="flex flex-col items-center justify-center">
                 <div className="card card-compact w-96 bg-base-100 shadow-xl mt-4">
                     <div className="card-body items-center">
-                        <span className="countdown font-mono text-5xl">
+                        <MyTimer expiryTimestamp={time} />
+                        {/* <span className="countdown font-mono text-5xl">
                             <span style={{ "--value": 10 } as CSSProperties}></span>:
                             <span style={{ "--value": 24 } as CSSProperties}></span>:
                             <span style={{ "--value": 27 } as CSSProperties}></span>
-                        </span>
+                        </span> */}
                     </div>
                 </div>
                 <ExerciseCard exerciseNumber="1" exercise='Running' duration='24'/>
@@ -31,3 +35,4 @@ const startWorkout = () => {
 };
 
 export default startWorkout;
+
