905722abc51eef786bdb1fd1b63589e04d6e3ebb
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2024-03-21 13:03:48 -0700

    added signup page and updated routing

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -9,7 +9,7 @@ const signup = () => {
       <div className="hero min-h-screen max-w-[75%] mx-auto">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-5xl font-bold">Sign Up!</h1>
             <p className="py-6">
               I'm thrilled to welcome you to our workout app, where your journey
               to fitness begins! Get ready to embark on a transformative
