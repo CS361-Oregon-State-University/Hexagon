b8b342cd4e7cc8379d11506608223721160ef547
Aiden Murphy <aidenjmurph@gmail.com>
2024-03-20 17:55:05 -0700

    fleshed out faq

--- client/src/routes/faq.tsx
+++ client/src/routes/faq.tsx
@@ -1,4 +1,6 @@
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import FAQPost from "../components/faqPosts";
 
 const faq = () => {
   return (
@@ -6,62 +8,44 @@ const faq = () => {
       <Navbar />
       <main className="flex flex-col items-center">
         <section>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" defaultChecked />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">
-              Click to open this one and close others
-            </div>
-            <div className="collapse-content">
-              <p>hello</p>
+          <FAQPost question="How do I reset my password?" answer="Go to the settings page and click on 'Reset Password'." category="Software" />
+          <FAQPost question="How do I start a workout?" answer="Go to the 'Start Workout' page and click on 'Start Workout'." category="Software" />
+          <FAQPost question="What workout plans are available?" answer="We offer a variety of plans including Cardio, Weightlifting, and Calisthenics." category="Workout" />
+          <FAQPost question="I can't find the workout I want to do." answer="Check your filters, make sure you aren't accidentally leaving that exercise out of your search. If you still cannot find the exercise you want, " category="Software" />
+          <FAQPost question="How do I start a workout?" answer="Go to the 'Start Workout' page and click on 'Start Workout'." category="Software" />
+          <FAQPost question="What workout plans are available?" answer="We offer a variety of plans including Cardio, Weightlifting, and Calisthenics." category="Workout" />
+        </section>
+        <section>
+          <div className="hero w-screen bg-base-200 mx-10 mt-10">
+            <div className="hero-content flex-col">
+              <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Can't find your question?</h1>
+              </div>
+              <div className="card shrink-0 w-screen max-w-4xl shadow-2xl bg-base-100">
+                <form className="card-body">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input type="text" placeholder="Type here" className="input input-bordered input-lg w-full" />                    <label className="label">
+                    </label>
+                  </div>
+                  <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </section>
       </main>
+      <Footer />
     </>
   );
 };
